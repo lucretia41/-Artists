99baf272d62945b2890dcb24334e0d1af9afb36d
Ian Scott Martin <ianscottmartin@gmail.com>
2023-05-12 19:11:48 -0600

    add consts

---  main.js
+++  main.js
@@ -5,3 +5,12 @@ fetch("http://localhost:3000/artdata")
     showCaseArtist([0]),
     console.log(artData)
 })
+
+const artistDisplay = document.querySelector("#artists-display")
+const artistName = documewnt.querySelector("name")
+const artistdate = document.querySelector("date")
+const artistImage = document.querySelector(".detail-image")
+const artistTitle = document.querySelector("title")
+
+
+
