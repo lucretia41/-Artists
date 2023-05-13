0f5bfac6da40765a5274e8d7f62067afa27ee379
Ian Scott Martin <ianscottmartin@gmail.com>
2023-05-12 19:25:30 -0600

    add fucntion to fetch each artist

---  main.js
+++  main.js
@@ -1,7 +1,7 @@
 fetch("http://localhost:3000/artdata")
 .then(res => res.json())
 .then(artData =>  {
-    artistsDisply(artData),
+    artistsDisplay(artData),
     showCaseArtist([0]),
     console.log(artData)
 })
@@ -11,6 +11,22 @@ const artistName = documewnt.querySelector("name")
 const artistdate = document.querySelector("date")
 const artistImage = document.querySelector(".detail-image")
 const artistTitle = document.querySelector("title")
+
+function artistsDisplay(artwork){
+    artData.forEach(artData => {
+        const eachArtist = document.createElement("img")
+        eachArtist.src = artistImage
+        artistDisplay.appendChild(eachArtist)
+        eachArtist.addEventListener('click', event => {
+            showCaseArtist(artwork)
+        })
+        eachArtist.addEventListener('mouseover', event => {
+            addGlow(event, eachArtist)
+        })
+        
+    });
+}
+
 
 
 
