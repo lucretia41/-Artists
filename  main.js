ff1380fdf9ba4239b55263ae66fd23238837a263
Ian Scott Martin <ianscottmartin@gmail.com>
2023-05-20 15:24:46 -0600

    notworking

---  main.js
+++  main.js
@@ -1,74 +1,140 @@
-fetch("http://localhost:3000/artdata")
-.then(res => res.json())
-.then(artData =>  {
-    artistsDisplay(artData),
-    showCaseArtist([0]),
-    console.log(artData)
+
+document.addEventListener('DOMContentLoaded', () => {
+    displayArtists();
+    addSubmitListener();
+
 })
 
-const artistDisplay = document.querySelector("#artists-display")
-const artistName = documewnt.querySelector("name")
-const artistdate = document.querySelector("date")
-const artistImage = document.querySelector(".detail-image")
-const artistTitle = document.querySelector("title")
-
-function artistsDisplay(artData){
-    artData.forEach(artwork => {
-        const eachArtist = document.createElement("img")
-        eachArtist.src = artistImage
-        artistDisplay.appendChild(eachArtist)
-        eachArtist.addEventListener('click', event => {
-            showCaseArtist(artwork)
-        })
-        eachArtist.addEventListener('mouseover', event => {
-            addGlow(event, eachArtist)
-        })
-        
-    });
+function displayArtists() {
+    fetch("http://localhost:3000/artdata")
+        .then(res => res.json())
+        .then(artists => {
+            artists.forEach(artist => renderOneArtist(artist))
+            // console.log(artists)
+            showArtistDetails(artists[0])
+        });
 }
-function addGlow(event, artistImage) {
-    const colors = ['red', 'blue', 'yellow', 'green'];
+
+function addSubmitListener() {
+    const artistForm = document.getElementById("new-artist");
+
+    artistForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        addNewArtist()
+        artistForm.reset();
+    })
+}
+
+function renderOneArtist(artist) {
+    const artistImg = document.createElement('img');
+    const artistDiv = document.createElement('div');
+    const artistMenu = document.getElementById("artist-menu");
+
+    artistImg.src = artist.image;
+
+
+    artistMenu.append(artistDiv);
+    artistDiv.append(artistImg);
+
+    artistImg.addEventListener("mouseover", () => showArtistDetails(artist));
+
+
+
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "delete";
+    deleteButton.className = "delet-btn";
+    artistDiv.append(deleteButton);
+
+    deleteButton.addEventListener("click", () => deleteArtist(artist.id, artistDiv))
+
+
+}
+
+function addGlow(artists) {
+    const colors = ['red', 'blue', 'green'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
 
     artistImage.style.borderColor = randomColor;
     artistImage.style.borderWidth = '5px';
     artistImage.style.borderStyle = 'solid';
-    artistImage.style.transiton = 'border-color 0.5s, border-width 0.5s';
+    artistImage.style.transition = 'border-color 0.5s, border-width 0.5s';
 
-    setTimeout(() =>{
+    setTimeout(() => {
         artistImage.style.borderColor = '';
         artistImage.style.borderWidth = '';
         artistImage.style.borderStyle = '';
-        artistImage.style.transiton = '';
-
+        artistImage.style.transition = '';
     }, 1000);
 }
 
-function addGlowToMainArtists(event, artistImage) {
-    const colors = ['red', 'blue', 'yellow', 'green'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
 
-    artistImage.style.borderColor = randomColor;
-    artistImage.style.borderWidth = '5px';
-    artistImage.style.borderStyle = 'solid';
-    artistImage.style.transiton = 'border-color 0.5s, border-width 0.5s';
+function showArtistDetails(artist) {
+    const detailImage = document.getElementById("detail-image");
+    const detailName = document.getElementById("detail-name");
+    // const detailRestaurant = document.getElementById("detail-restaurant");
+    const detailDate = document.getElementById("detail-date");
+    const detailTitle = document.getElementById("detail-title");
+
+
+    detailImage.src = artist.image;
+    detailName.textContent = artist.name;
+    // detailRestaurant.textContent = ramen.restaurant;
+    detailDate.textContent = artist.date;
+    detailTitle.textContent = artist.title;
+
 
 }
 
-function showCaseArtist(){
-    artistName.textContent = artist.artistName
-    artistdate.textContent = artist.date    
-    artistImage.src = artistImage
- artistImage.addEventListener('mouseover', event => {
-    addGlowToMainArtists (event, artistImage);
+function addNewArtist() {
+    const newName = document.getElementById("new-name").value;
+    // const newRestaurant = document.getElementById("new-restaurant").value;
+    const newImage = document.getElementById("new-image").value;
+    const newDate = document.getElementById("new-date").value;
+    const newTitle = document.getElementById("new-title").value;
 
- })   
+    const newArtist = {
+        "name": newName,
+        // "restaurant": newRestaurant,
+        "image": newImage,
+        "date": newDate,
+        "title": newTitle
+    }
+    fetch("http://localhost:3000/artdata", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newArtist)
+    })
+
+    renderOneArtist(newArtist);
+
+    // display the details of the new ramen (nifty!)
+    showArtistDetails(newArtist);
 }
 
-console.log()
 
+function deleteArtist(id, artistDiv) {
+    // delete ramen from database
+    fetch(`http://localhost:3000/artists/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
 
+    // delete corresponding ramen image from menu
+    artistDiv.remove();
 
+    // reset the displayed ramen info
+    const placeholderInfo = {
+        "name": "Click an Artist!",
+        // "restaurant": ":3",
+        "image": "",
+        "date": "Select a artist to display!",
+        "title": "Same deal."
+    }
 
-
-
+    showArtistDetails(placeholderInfo);
+}
